1b41737509f4f7424cdc708c77776e141b022d29
苗大 <ufo025174@gmail.com>
2023-05-09 21:52:54 +0800

    wip: nginx只读模式

--- src/components/Statusbar.vue
+++ src/components/Statusbar.vue
@@ -6,22 +6,6 @@
         <span class="ml-1">{{ selectedItemCount > 0 ? selectedItemCount + ' ' + t('item(s) selected.') : '' }}</span>
       </div>
     </div>
-    <div class="flex leading-5 items-center">
-      <select
-        v-model="locale"
-        @change="changeLocale($event.target.value)"
-        class="py-0.5 text-sm text-slate-500 dark:text-neutral-50 dark:bg-gray-700 rounded pl-2 pr-8 mr-3"
-      >
-        <option value="" disabled>{{ t('Language') }}</option>
-        <option value="zh-cn">Chinese</option>
-        <option value="en">English</option>
-        <option value="fr">French</option>
-        <option value="fa">Persian</option>
-        <option value="ru">Russian</option>
-        <option value="tr">Turkish</option>
-        <option value="tr">Hebrew</option>
-      </select>
-    </div>
   </div>
 </template>
 
