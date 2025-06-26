d22e67f1cec9308a9a42502f1918464d3c890330
liuchaoyi <ih_liuchaoyi>
2025-06-26 12:06:39 +0800

    feat: 文件查看窗口优化

---
+++ src/components/EllipsisMiddle.vue
@@ -0,0 +1,39 @@
+<template>
+  <div ref="textRef" class="flex items-center" v-bind="$attrs">
+    <span ref="ellipsisTextRef" class="inline-block truncate">
+      {{ text.slice(0, -getSliceNumber(text)) }}
+    </span>
+    <bdi dir="rtl" class="inline-block overflow-hidden whitespace-nowrap">
+      <bdi dir="ltr" style="margin-left: 2px;">{{ text.slice(-getSliceNumber(text)) }}</bdi>
+    </bdi>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+defineOptions({
+  name: 'EllipsisMiddle',
+});
+
+defineProps({
+  text: {
+    type: String,
+    required: true,
+  },
+
+});
+
+const tippyFunc = ref();
+const textRef = ref();
+const ellipsisTextRef = ref();
+
+// 获取截取的字符数
+function getSliceNumber(str: string) {
+  const num = Math.floor((str?.length || 3) / 3);
+
+  return num > 0 ? num : 1;
+}
+
+
+</script>
